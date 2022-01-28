04bd5e90afdc8c32d7156699f2403a91e1536fa8
CJBarahona <cbarahona@t-developing.com>
2022-01-28 13:50:31 -0600

    Creacion de clases

---
+++ Lab2P2_CarlosBarahona_12041015/src/lab2p2_carlosbarahona_12041015/Animales.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2p2_carlosbarahona_12041015;
+
+/**
+ *
+ * @author Admin
+ */
+public class Animales {
+
+    private String nombreCientifico;
+    private String nombreComun;
+    private String habitat;
+    private String alimentacion;
+    private String descripcionRasgos;
+    private String distribucionGeografica;
+    private int vida;
+
+    public Animales(String nombreCientifico, String nombreComun, String habitat, String alimentacion, String descripcionRasgos, String distribucionGeografica, int vida) {
+        this.nombreCientifico = nombreCientifico;
+        this.nombreComun = nombreComun;
+        this.habitat = habitat;
+        this.alimentacion = alimentacion;
+        this.descripcionRasgos = descripcionRasgos;
+        this.distribucionGeografica = distribucionGeografica;
+        this.vida = vida;
+    }
+
+    public String getNombreCientifico() {
+        return nombreCientifico;
+    }
+
+    public void setNombreCientifico(String nombreCientifico) {
+        this.nombreCientifico = nombreCientifico;
+    }
+
+    public String getNombreComun() {
+        return nombreComun;
+    }
+
+    public void setNombreComun(String nombreComun) {
+        this.nombreComun = nombreComun;
+    }
+
+    public String getHabitat() {
+        return habitat;
+    }
+
+    public void setHabitat(String habitat) {
+        this.habitat = habitat;
+    }
+
+    public String getAlimentacion() {
+        return alimentacion;
+    }
+
+    public void setAlimentacion(String alimentacion) {
+        this.alimentacion = alimentacion;
+    }
+
+    public String getDescripcionRasgos() {
+        return descripcionRasgos;
+    }
+
+    public void setDescripcionRasgos(String descripcionRasgos) {
+        this.descripcionRasgos = descripcionRasgos;
+    }
+
+    public String getDistribucionGeografica() {
+        return distribucionGeografica;
+    }
+
+    public void setDistribucionGeografica(String distribucionGeografica) {
+        this.distribucionGeografica = distribucionGeografica;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    @Override
+    public String toString() {
+        return "Animales{" + "nombreCientifico=" + nombreCientifico + ", nombreComun=" + nombreComun + ", habitat=" + habitat + ", alimentacion=" + alimentacion + ", descripcionRasgos=" + descripcionRasgos + ", distribucionGeografica=" + distribucionGeografica + ", vida=" + vida + '}';
+    }
+    
+    
+
+}
---
+++ Lab2P2_CarlosBarahona_12041015/src/lab2p2_carlosbarahona_12041015/Lab2P2_CarlosBarahona_12041015.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2p2_carlosbarahona_12041015;
+
+/**
+ *
+ * @author Admin
+ */
+public class Lab2P2_CarlosBarahona_12041015 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+                Animales pinguino = new Animales( "Spheniscidae", "Pingüino", "Areas heladas", "Peces", "Pequeños con un pico anaranjado y con color negro en su cuerpo y blanco en su estomago", "Hemisferio Sur", 50);
+                   Animales leon = new Animales( "Panthera leo", "León", "Sabanas", "Carne", "Tienen una melena que cubre su cabeza y son grandes con dientes filudos", "África ", 150);
+                   Animales elefante = new Animales( "Elephantidae", "Elefante", "Selva tropical", "Hierba", "Son gigantes con cuernos cerca de sus bocas con un trompa gigante y orejas gigantes", "África ", 250);
+    
+    }
+    
+}
